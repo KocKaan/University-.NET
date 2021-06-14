f466cb228e59dafe7d6369cf564c681e478af5cf
KocKaan <kk4291@nyu.edu>
2021-06-14 16:16:46 +0300

    Added the About Page

--- KarakasUniversity/Controllers/HomeController.cs
+++ KarakasUniversity/Controllers/HomeController.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KarakasUniversity.DAL;
+using KarakasUniversity.ViewModels;
 
 namespace KarakasUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+            private SchoolContext db = new SchoolContext();
+            public ActionResult Index()
         {
             return View();
         }
 
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //The LINQ statement groups the student entities by enrollment date, calculates the number of
+            //entities in each group, and stores the results in a collection of EnrollmentDateGroup view model objects.
 
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +38,12 @@ namespace KarakasUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
