d42d57f528f1d00e9875aba87ac4b82f55a6b530
KocKaan <kk4291@nyu.edu>
2021-06-15 11:42:36 +0300

    Added Student Service and moved the database to another file.

--- KarakasUniversity/Models/Interfaces/SchoolContext.cs
+++ KarakasUniversity/Models/Interfaces/SchoolContext.cs
@@ -11,6 +11,7 @@ namespace KarakasUniversity.DAL
         {
         }
 
+
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
---
+++ KarakasUniversity/Services/StudentServices.cs
@@ -0,0 +1,26 @@
+﻿using KarakasUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KarakasUniversity.Services
+{
+
+    public class StudentServices
+    {
+
+
+        public Student getStudentDetails()
+        {
+
+        }
+
+
+
+
+
+    }
+
+
+}
