d6e640e0cae3c7bbe9f122bd7cb9cda60e43cdf4
KocKaan <kk4291@nyu.edu>
2021-07-01 18:03:50 +0300

    Added test Project

---
+++ KarakasUniversity.Tests/Services/StudentServicesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KarakasUniversity.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakasUniversity.Services.Tests
+{
+    [TestClass()]
+    public class StudentServicesTests
+    {
+        [TestMethod()]
+        public void postStudentDeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
