0ec38884fa997839d3052bc63e7886f9d33412a3
KocKaan <kk4291@nyu.edu>
2021-07-02 17:42:45 +0300

    Created the SetUp constructor

--- KarakasUniversity.Tests/Services/StudentServicesTests.cs
+++ KarakasUniversity.Tests/Services/StudentServicesTests.cs
@@ -5,12 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using KarakasUniversity.Models.Interfaces;
+using KarakasUniversity.Services.Interfaces;
+using KarakasUniversity.Model.Entities;
+using Moq;
 
 namespace KarakasUniversity.Services.Tests
 {
-    [TestClass()]
+    [TestFixture]
     public class StudentServicesTests
     {
+        private IStudentService _studentService;
+        private Mock<ISchoolContext> _schoolContext;
+      
+        [SetUp]
+        public void Init()
+        {
+            _schoolContext = new Mock<ISchoolContext>();
+            _studentService = new StudentServices(_schoolContext.Object);
+        }
+
         [TestMethod()]
         public void postStudentDeleteTest()
         {
